9d8ba7770117d1ce083a32461d562f332d66ee52
RobinDadswell <19610103+RobinDadswell@users.noreply.github.com>
2026-02-17 14:28:15 +0000

    New: Added Covers to the Progress page

--- src/PS2Challenge.Backend/Models/GameProgressDto.cs
+++ src/PS2Challenge.Backend/Models/GameProgressDto.cs
@@ -3,7 +3,9 @@ namespace PS2Challenge.Backend.Models;
 public class GameProgressDto
 {
     public int ProgressId { get; set; }
+    public int GameId { get; set; }
     public string GameTitle { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
     public DateOnly DateStarted { get; set; }
     public DateOnly? DateFinished { get; set; }
     public TimeSpan? CompletionTime { get; set; }
