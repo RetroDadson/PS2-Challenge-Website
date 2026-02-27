6741a14d36303b719b3e6e0ed7cb6029a48a181e
RobinDadswell <19610103+RobinDadswell@users.noreply.github.com>
2026-02-27 21:46:04 +0000

    fix: resolve remaining SonarCloud open issues

--- src/PS2Challenge.Main/Api/Hubs/VotesHub.cs
+++ src/PS2Challenge.Main/Api/Hubs/VotesHub.cs
@@ -4,4 +4,13 @@ namespace PS2Challenge.Main.Api.Hubs;
 
 public class VotesHub : Hub
 {
+	public string GetConnectionId()
+	{
+		return Context.ConnectionId;
+	}
+
+	public Task Ping()
+	{
+		return Clients.Caller.SendAsync("Pong");
+	}
 }
